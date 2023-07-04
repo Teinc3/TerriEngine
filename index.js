87e3f928030e43226afb6f5d04022c55e6d19f7e
Teinc3 <s17415@lsc.hk>
2023-07-05 05:31:07 +0800

    Wrote some code but i don't think it works at all, just saving it here for later

--- index.js
+++ index.js
@@ -282,3 +282,89 @@ var algo = new Algo, interest = new Interest, pixel = new Pixel, processAction =
 
 time.init();
 time.update();
+
+/* Task:
+1. Obtain a completely random infosend (IFS) tick as initial processAtk tick
+2. Calculate the number of IFSes and their respective ticks, with the range starting from initial tick all the way to before the next cycle starts
+3. Repeatedly test combinations of these IFS by enabling/disabling them as reinforcement ticks through binary (0000 -> 0001 -> 0010 etc).
+4. Calculate the ticks for an atk update (AU). For each "Enabled" IFS, obtain the next closest AU tick and stick it to the IFS object.
+5. Calculate the number of AUs between each enabled IFS. This allows the amount of troops required during each IFS to be easily calculated.
+6. Execute simulation. If running out of troops, terminate the run and continue with a different combination.
+7. Log all final results in a JSON file.
+*/
+
+function generateCombinations(IFSes, combinations, currentIndex) {
+    if (currentIndex === IFSes.length) {
+        // All elements in combinations are set, do something with the combination
+        // Valid combination found, now we map the combination back to the IFSes array (overwrite)
+        // We make an object containing attributes for IFS and next closest AU tick first.
+        // We can then use the objects to calculate the number of AUs between each IFS later.
+        IFSes = IFSes.filter(index => combinations[index]).map(IFS => {
+            return {
+                IFS: IFS,
+                CAUT: 0, // Closest next AU tick. For now, we just set it to 0,
+                auDiffs: 0, // Number of AUs between this and next IFS/cyc end. For now, we just set it to 0
+                troops: 0 // Number of troops required for this IFS. For now, we just set it to 0
+            }
+        });
+        // Now we calculate the closest AU tick for each IFS
+        var auInterval = pixel.getLand() < 1E3 ? 4 : 3
+        IFSes.forEach(IFS => {
+            var closestAU = initIFS + 7;
+            while (closestAU < IFS.IFS) {
+                closestAU += auInterval;
+            }
+            IFS.CAUT = closestAU;
+        })
+        // Task 5, Now we calculate the number of AUs between each IFS and store it in the auDiffs attribute.
+        // For the last IFS we calculate the number of AUs between it and the end of the cycle (tick 98, inclusive!)
+        for (var j = 0; j <= IFSes.length - 1; j++) {
+            IFSes[j].auDiffs = (IFSes[j + 1].CAUT - IFSes[j].CAUT)/auInterval;
+        }
+        IFSes[IFSes.length - 1].auDiffs = Math.floor((98 - IFSes[IFSes.length - 1].CAUT)/auInterval); //Check if floor or ceil
+        // Now calculate the number of land (and troops) required for each IFS
+        // We start from the first IFS and work our way forward
+        var landDiff = 0, nextExpansion = (Math.sqrt(2*pixel.getLand() + 1)/2 - .5)*4;
+        IFSes.forEach(IFS => {
+            landDiff = 0;
+            // We Repeat this for auDiffs times: (add nextExpansion to landDiff, then nextExpansion += 4)
+            for (var k = 0; k < IFS.auDiffs; k++) {
+                landDiff += nextExpansion;
+                nextExpansion += 4;
+            }
+            // Then apply amount to troops
+            IFS.troops = 2 * landDiff + nextExpansion - 4;
+        })
+
+        // Now we execute the simulation
+        time.init();
+        var simEnd = false;
+        while (!simEnd) simEnd = time.update();
+        return;
+    }
+  
+    // Toggle IFS state to 0 (disable)
+    combinations[currentIndex] = 0;
+    generateCombinations(IFSes, combinations, currentIndex + 1);
+  
+    // Toggle IFS state to 1 (enable)
+    combinations[currentIndex] = 1;
+    generateCombinations(IFSes, combinations, currentIndex + 1);
+  }
+
+function testLoop() {
+    for (var initIFS = 56; initIFS <= 91; initIFS+=7) { // Task 1
+        var IFSes = [], currentIFS = initIFS + 7;
+        while (currentIFS < 100) { // Task 2
+            IFSes.push(currentIFS);
+            currentIFS += 7;
+        }
+        // Now we generate all possible binary combinations of enable/disable IFSes
+        // For example, if the array is [0,0,0], the combinations will be [0,0,0], [0,0,1], [0,1,0], [0,1,1] etc.
+        var combinations = Array(IFSes.length).fill(0);
+        // Loop from the last element to the first element, toggle if needed and recursion until all combinations are generated
+        generateCombinations(IFSes, combinations, 0);
+    }
+}
+
+testLoop();
